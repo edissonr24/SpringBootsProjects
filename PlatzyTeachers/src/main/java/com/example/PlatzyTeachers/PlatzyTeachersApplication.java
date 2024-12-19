fc4da8d82dd9a6956932518e44adc903c7ea1d55
edissonr24 <edissonr24@gmail.com>
2024-12-18 20:42:08 -0500

    first commit

---
+++ PlatzyTeachers/src/main/java/com/example/PlatzyTeachers/PlatzyTeachersApplication.java
@@ -0,0 +1,13 @@
+package com.example.PlatzyTeachers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatzyTeachersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatzyTeachersApplication.class, args);
+	}
+
+}
---
+++ PlatzyTeachers/src/test/java/com/example/PlatzyTeachers/PlatzyTeachersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlatzyTeachers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatzyTeachersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
